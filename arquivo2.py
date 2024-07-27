be61e7646b9274b4b460126fd247aeb30794d3f6
Gabriel Mascarenhas <gabrielcmascarenhas@gmail.com>
2024-07-27 15:45:04 -0300

    Segunda Versão

--- arquivo2.py
+++ arquivo2.py
@@ -1,6 +1,14 @@
 #GIT INIT
 
+#Quando faz git clone não precisa fazer git init, vai utilizar o git init quando
+# cria uma pasta no computador e não tem o git configurado
+
+# git clone se quiser só importar os arquivos e n a pasta com os arquivos, só colocar um espaço. --> .
+
+#git add arquivo1 arquivo2... para ter vários arquivos atualizados
+#Se quiser adicionar todos os aquivos de uma vez é git add .
 #git status
 
 def ordena(x):
     return x
+
