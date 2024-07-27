c0bc497854b37f17b16ae7d27339f5537143158f
Gabriel Mascarenhas <gabrielcmascarenhas@gmail.com>
2024-07-27 00:25:42 -0300

    Primeiro Projeto

---
+++ arquivo1.py
@@ -0,0 +1,10 @@
+lista = [1, 324,453, 56, 32, 21, 6546, 13, 5454, 6123, 654, 212, 4578, 2344, 634, 3, 57, 58]
+
+lista.sort()
+print(lista)
+
+#cd (change diretory) --> cd <caminho>
+
+#ls (list) --> pd-se usar ls -al (para ver os arquivos ocultos) no git bash
+#Já no powershell usa-se Get-ChieldItem (ls) e Get-ChieldItem(para ls -al) pelo q entendi
+
